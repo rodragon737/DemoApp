b2bad9b80bdaa0637a659d0a78eb29bbb4b9c570
Rodrigo Martin Rosales <rosales.rodrigo.martin@gmail.com>
2023-08-15 20:09:21 -0300

    cambio de Nombre del directorio

---
+++ Rodrigo/Api/Modelo/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Modelo
+{
+    public class ToDo
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string userMail { get; set; }
+    }
+}
